51c297c3f2c57c2eab6c2ae710fde756aa716eeb
Hedriss10 <hedrisgts@gmail.com>
2023-01-11 15:01:20 -0300

    Frist commit one codes java

---
+++ HelloWord/src/com/java/Boleano.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class Boleano {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int idade = 18;
+		
+		boolean checa = idade == 18;
+		
+		System.out.println(checa);
+		
+	}
+
+}
